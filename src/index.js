c4798a2b68509aae357319e0b996f6a643c3351e
RomanovRoma <romanian.reget@gmail.com>
2020-12-24 10:08:53 +0300

    check: check fetch api swapi.dev

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello Star Wars!')
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
+
+getResource('https://swapi.dev/api/people/1/')
+  .then((body) => {
+    console.log(body);
+  });
