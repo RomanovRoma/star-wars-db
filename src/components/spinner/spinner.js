49a04d06667bb059184c40573ac1f3c6d4fbe46c
RomanovRoma <romanian.reget@gmail.com>
2020-12-28 14:35:30 +0300

    feat: create component Spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-nvl5o5qy8d">
+      <div className="ldio-gx58oecry9o">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner
