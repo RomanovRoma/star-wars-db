8cf59760aae73b43dd5d740f3ebb3dc5ab33a084
RomanovRoma <romanian.reget@gmail.com>
2020-12-31 13:11:45 +0300

    feat: add component ErrorButton and componentDidCatch() in App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,12 +6,15 @@ import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 
 import "./app.css";
+import ErrorButton from "../error-button";
+import ErrorIndicator from "../error-indicator";
 
 export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: null
+    selectedPerson: null,
+    hasError: false
   }
 
   toggleRandomPlanet = () => {
@@ -28,7 +31,16 @@ export default class App extends Component {
     })
   }
 
+  componentDidCatch() {
+    console.log('componentDidCatch()')
+    this.setState({hasError: true})
+  }
+
   render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
 
     const planet = this.state.showRandomPlanet ?
       <RandomPlanet /> :
@@ -39,11 +51,14 @@ export default class App extends Component {
         <Header />
         { planet }
 
-        <button
-          className="toggle-planet btn btn-warning btn-lg"
-          onClick={this.toggleRandomPlanet}>
-          Toggle Random Planet
-        </button>
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+          <ErrorButton />
+        </div>
 
 
         <div className="row mb2">
