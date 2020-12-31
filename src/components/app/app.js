9fac5ebd6d410d593ade5219fef9fa835b1f37f3
RomanovRoma <romanian.reget@gmail.com>
2020-12-31 19:32:01 +0300

    refactor: pass function swapiServicein ItemList as data sources

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,8 +7,13 @@ import "./app.css";
 import ErrorButton from "../error-button";
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
+import SwapiService from "../../services/swapi-service";
 
 export default class App extends Component {
+
+  swapiService = new SwapiService()
 
   state = {
     showRandomPlanet: true,
@@ -41,21 +46,44 @@ export default class App extends Component {
     return (
       <div className="stardb-app">
         <Header />
-        { planet }
+        {planet}
 
         <div className="row mb2 button-row">
           <button
             className="toggle-planet btn btn-warning btn-lg"
-            onClick={this.toggleRandomPlanet}>
+            onClick={this.toggleRandomPlanet}
+          >
             Toggle Random Planet
           </button>
           <ErrorButton />
         </div>
 
         <PeoplePage />
-        <PeoplePage />
-        <PeoplePage />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 };
