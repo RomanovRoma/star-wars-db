d3ae0cbe52217ce6a822d9eafb042d68a9ed58a0
RomanovRoma <romanian.reget@gmail.com>
2020-12-31 22:41:13 +0300

    refactor: add element Row in component PeoplePage

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -6,6 +6,14 @@ import PersonDetails from '../person-details';
 
 import './people-page.css'
 
+const Row = ({ left, right }) => {
+  return (
+    <div className="row mb2">
+      <div className="col-md-6">{left}</div>
+      <div className="col-md-6">{right}</div>
+    </div>
+  );
+}
 export default class PeoplePage extends Component {
 
   swapiService = new SwapiService()
@@ -35,18 +43,22 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />
     }
 
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({ name, gender, birthYear }) => (
+          `${name} (${gender}, ${birthYear})`)} />
+    );
+
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={({name, gender, birthYear}) => `${name} (${gender}, ${birthYear})`}
-          />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
+      <div>
+        <Row left={itemList} right={personDetails} />
+        <Row left={<p>Star</p>} right={<span>Wars</span>} />
       </div>
     );
   }
