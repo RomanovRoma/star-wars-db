be8fa94f9d3e71d3326a3fd9ed6cb2f45691a452
RomanovRoma <romanian.reget@gmail.com>
2021-01-13 21:59:39 +0300

    feat: add optional parameters PeoplePage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -48,7 +48,7 @@ export default class App extends Component {
               <Route  path="/people"
                       render={() => <h2>People</h2>}
                       exact />
-              <Route  path="/people" component={PeoplePage} />
+              <Route  path="/people/:id?" component={PeoplePage} />
               <Route  path="/planets"
                       render={() => <h2>Planets</h2>}
                       exact />
@@ -62,7 +62,6 @@ export default class App extends Component {
                         const { id } = match.params;
                         return <StarshipDetails itemId={id} />
                       }} />
-
             </div>
           </Router>
         </SwapiServiceProvider>
